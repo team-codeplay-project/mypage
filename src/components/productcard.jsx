05e46712a256b3f261eab8a982a2d07737facd77
daom bae <da6970@gmail.com>
2023-07-08 18:45:48 +0900

    래플/옥션카드

--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "../style/rafflebox.css";
 
 const ProductCard = ({ r_data }) => {
-  
+  const handleRaffleClick = () => {};
+
   return (
-    <div className="product-box" >
+    <div className="product-box">
       <div className="product-image"></div>
       <div className="product-info">
         <h3>{r_data.id} 번 래플</h3>
         <p>{r_data.URL} 블럭부터 추첨 </p>
+        <button className="raffle-button" onClick={handleRaffleClick}>
+          응모하기
+        </button>
       </div>
     </div>
   );
